4a71a0aef791e65aed21c16df195482245a3d8ae
gafalcon <gafalcon@espol.edu.ec>
2016-12-19 17:09:45 -0500

    Autocomplete in web

--- custom/config-web.el
+++ custom/config-web.el
@@ -28,5 +28,25 @@
 ;; web-mode engines
 (setq web-mode-engines-alist '(("php" . "\\.phtml\\'") ("blade" . "\\.blade\\.")) )
 
+(use-package company-web-html
+  :config
+  (add-hook 'web-mode-hook (lambda ()
+			     (set (make-local-variable 'company-backends) '(company-tern (company-web-html company-yasnippet company-dabbrev-code)))
+			     ))
+  ;; Enable JavaScript completion between <script>...</script> etc.
+  (defadvice company-tern (before web-mode-set-up-ac-sources activate)
+    "Set `tern-mode' based on current language before running company-tern."
+    (message "advice")
+    (if (equal major-mode 'web-mode)
+	(let ((web-mode-cur-language
+	       (web-mode-language-at-pos)))
+        (if (or (string= web-mode-cur-language "javascript")
+                (string= web-mode-cur-language "jsx")
+                )
+            (unless tern-mode (tern-mode))
+          (if tern-mode (tern-mode -1))))))
+  
+  
+  )
 (provide 'config-web.el)
 ;;; config-web.el ends here
